17cd45124af4d6916891330d986ff86b21a5cfb3
ETCraig <dev.etcraig@gmail.com>
2026-03-07 22:06:12 -0600

    refactor: updated actions and added  initial topics components

---
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -0,0 +1,8 @@
+export default function PostShowPage() {
+  return (
+    <div>
+      <h1>Post ID: </h1>
+      <p>Belongs to topic: </p>
+    </div>
+  );
+}
---
+++ src/app/topics/[slug]/posts/new/page.tsx
@@ -0,0 +1,3 @@
+export default function PostCreatePage() {
+  return <div>Create a new post for topic: </div>;
+}
